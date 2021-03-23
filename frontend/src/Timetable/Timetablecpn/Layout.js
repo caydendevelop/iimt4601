628f32d9745d15f8475a50f32945fa4be0c22191
KazuRaitoNin <75256232+KazuRaitoNin@users.noreply.github.com>
2021-03-23 21:01:51 +0800

    Update Layout.js

--- frontend/src/Timetable/Timetablecpn/Layout.js
+++ frontend/src/Timetable/Timetablecpn/Layout.js
@@ -9,14 +9,6 @@ function Layout1() {
     return(
     <>
     <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['3']}>
-        <Menu.Item key="1">Course Information</Menu.Item>
-        <Menu.Item key="2">Course Review</Menu.Item>
-        <Menu.Item key="3">Personal Timetable</Menu.Item>
-      </Menu>
-    </Header>
     <Content style={{ padding: '0 50px', background: '#fefbefff' }}>
       <div className="site-layout-content">
       <div id="left">
