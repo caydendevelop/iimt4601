d1437bac4ef595745a4cca4715cadb04bbd18b1b
KazuRaitoNin <75256232+KazuRaitoNin@users.noreply.github.com>
2021-03-23 18:23:28 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import NavBar from './NavBar'
 import 'antd/dist/antd.css';
 import CList from './CList/CList'
-import CourseReviewFront from './CourseReviewFront/ReviewFront'
+import ReviewFront from './ReviewFront/ReviewFront'
 import CReview from './CReview/CReview'
 import FrontPage from './FrontPage/FrontPage.js'
-import LoginPage from './Login/LoginPage.js'
-import Page1 from './Page1/Page1'
-import Page9 from './Page9/Page9'
-import SignUpPage from './SignUp/SignUpPage'
+import LoginPage from './LoginPage/LoginPage.js'
+import Timetable from './Timetable/Timetable'
+import SignUp from './SignUp/SignUp'
+import AddReview from './AddReview/AddReview'
 
 import { Layout, Menu, Breadcrumb } from 'antd';
 
@@ -22,14 +22,13 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Route path="/CList" exact component={CList}/>
-        <Route path="/CourseReviewFront" exact component={CourseReviewFront}/>
+        <Route path="/ReviewFront" exact component={ReviewFront}/>
         <Route path="/CReview" exact component={CReview}/>
         <Route path="/" exact component={FrontPage}/>
         <Route path="/LoginPage" exact component={LoginPage}/>
-        <Route path="/Page1" exact component={Page1}/>
-        <Route path="/Page9" exact component={Page9}/>
-        <Route path="/Page9" exact component={Page9}/>
-        <Route path="/SignUpPage" exact component={SignUpPage}/>
+        <Route path="/Timetable" exact component={Timetable}/>
+        <Route path="/AddReview" exact component={Addreview}/>
+        <Route path="/SignUpPage" exact component={SignUp}/>
 
         
         
